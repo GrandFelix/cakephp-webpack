ff0bc2a99d29086d049c5100db7a66656f020601
Blaz <bsmolnikar@icloud.com>
2022-10-22 21:34:56 +0200

    config load fix

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,6 +5,5 @@ use \Cake\Core\Configure;
 $isCli = PHP_SAPI === 'cli';
 
 if ($isCli) {
-    Configure::load('Webpack.config');
-//    Configure::load('GrandFelix/Webpack.webpack_main');
+    Configure::load('GrandFelix/Webpack.config');
 }
